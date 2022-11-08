1f813ef98f14257abde93b970facb0237f99bf7d
sdivakaran143 <sdivakaran683@gmail.com>
2022-11-08 19:44:43 +0530

    08.11.2022

--- js.js
+++ js.js
@@ -1,5 +1,9 @@
 function actionn(id){
-    document.getElementById(id).style.transform='translateY(0px)';
+    if(document.getElementById(id).style.transform='translateY(0px)'=="translateY('0px')"){
+        document.getElementById(id).style.transform='translateY(0px)';
+    }
+    document.getElementById(id).style.transform='translateY(20px)';
     // alert("sdifg");
     console.log("DSAfsdfsdfsd");
 }
+console.log(document.getElementById("fn").style.transform='translateY(0px)');
