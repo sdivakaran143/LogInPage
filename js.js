9e8a2a38069f6aa7dde37e5f16da693951823e53
sdivakaran143 <sdivakaran683@gmail.com>
2022-11-09 19:30:41 +0530

    9.11.2022

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
 function actionn(id){
-    if(document.getElementById(id).style.transform='translateY(0px)'=="translateY('0px')"){
-        document.getElementById(id).style.transform='translateY(0px)';
-    }
-    document.getElementById(id).style.transform='translateY(20px)';
-    // alert("sdifg");
-    console.log("DSAfsdfsdfsd");
+    document.getElementById(id).style.transform='translateY(0px)';
 }
-console.log(document.getElementById("fn").style.transform='translateY(0px)');
+function page2(id){
+    document.getElementById(id).style.transform='translateY(20px)';
+}
+function goto(){
+    Window.open("techcommunity.microsoft.com/t5/image/serverpage/image-id/22771i53EF94F903D2CEB0/image-dimensions/1500?v=v2&px=-1","_blank");
+}
